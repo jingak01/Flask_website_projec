bad6f94d7fd3586431d7ecb6adbed14aff95c5e0
Akshay jingar <raja.jingar@yahoo.com>
2023-03-19 19:52:52 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def Index():
+    First_name = 'Akshay jingar'
+    stuff = 'This is <strong>random stuff</strong> i created for website.'
+    Fav_topping = ['Paneer','Chili','cheese','carrot','jelepino',41]
+
+    return render_template('index.html', 
+                            First_name = First_name,
+                            stuff = stuff,
+                            Fav_topping = Fav_topping
+                            )
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template('user.html',user_name = name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_issue():
+    return render_template('500.html'),500
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
