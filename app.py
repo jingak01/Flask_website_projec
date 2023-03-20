eacf090b29d513afc578171b1267c3ad476696c3
Akshay jingar <raja.jingar@yahoo.com>
2023-03-20 19:07:10 +0530

    adding form in the application

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-@app.route("/")
 
+app.config['SECRET_KEY'] = 'My flask application secret key'
+#create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name?",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route("/")
 def Index():
     First_name = 'Akshay jingar'
     stuff = 'This is <strong>random stuff</strong> i created for website.'
@@ -14,6 +23,23 @@ def Index():
                             stuff = stuff,
                             Fav_topping = Fav_topping
                             )
+
+
+
+@app.route("/name",methods=['GET','POST'])
+def name():
+    name = None
+    Form=NamerForm()
+
+    #validate Form
+    if Form.validate_on_submit():
+        name = Form.name.data
+        Form.name.data = ''
+
+    return render_template('name.html',
+                           name = name,
+                           Form = Form)
+
 
 @app.route("/user/<name>")
 def user(name):
@@ -27,6 +53,9 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_error():
     return render_template('500.html'),500
+
+#create a form class
+
 
 
 if __name__ == "__main__":
